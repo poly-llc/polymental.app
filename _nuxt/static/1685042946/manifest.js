12184b31d0e64ba3ed0372a439f7cce9b2e071c0
Matt Spinelli <matt@polymentalhealth.com>
2023-06-12 23:19:32 -0400

    v0.0.122

--- _nuxt/static/1685042946/manifest.js
+++
@@ -1,1 +0,0 @@
-__NUXT_JSONP__("manifest.js", {routes:["\u002F","\u002Fsignup\u002Fexplorer","\u002Flogin","\u002Fsignup","\u002Fanalytics","\u002Fcertification","\u002Fsignup\u002Fdemo","\u002Fsignup\u002Fforecaster"]})
---
+++ _nuxt/static/1686623974/manifest.js
@@ -0,0 +1,1 @@
+__NUXT_JSONP__("manifest.js", {routes:["\u002F","\u002Fsignup\u002Fforecaster","\u002Fsignup\u002Fexplorer","\u002Fsignup","\u002Fsignup\u002Fdemo","\u002Fcertification","\u002Flogin","\u002Fanalytics"]})
