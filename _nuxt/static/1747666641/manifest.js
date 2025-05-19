8e6a8b594b0d6fcfa26bc4eecc9f78c946a22276
Matt Spinelli <651748+mspinelli@users.noreply.github.com>
2025-05-19 11:23:42 -0400

    v0.0.152

---
+++ _nuxt/static/1747666641/manifest.js
@@ -0,0 +1,1 @@
+__NUXT_JSONP__("manifest.js", {routes:["\u002F","\u002Fanalytics","\u002Fsignup\u002Fdemo","\u002Flogin","\u002Fsignup","\u002Fcertification","\u002Fsignup\u002Fexplorer","\u002Fsignup\u002Fforecaster"]})
